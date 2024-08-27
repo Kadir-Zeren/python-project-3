f1f4c19e7f78b016afb8e0d6037325293d64b73c
kadir-zeren <k4dzeren@gmail.com>
2024-08-27 21:29:20 +0300

    project finished

--- python-project.py
+++ python-project.py
@@ -1,4 +1,117 @@
 print(11-7)
 print(4 + 11.0)
 print('11 - 7')
-print('4' + 4)
+print('clarusway')
+print('3+5')
+print ('abc')
+print('!-**asdflkasdjlfk')
+num1, num2 = 81, 55
+num3 = num1 - num2
+print(num3)
+a,b = 3, 5
+print(a,b)
+r = 5
+pi = 3.14
+area = pi * (r ** 2)
+print(area)
+print(11% 2) # remainder of this division is 1
+# it means 11 is an odd number
+print((45) / 2) # parentheses are used as in normal math operations
+print(2** 3)  # 2 to the power of 3
+print(3 ** 2) # square of 3
+a = 2
+b = 8
+print((a * b) ** 0.5) # square root
+a = 5
+a = a + 1
+a +=1
+A = 10
+A = A * 2
+A *= 2
+a = (1 + 1 ) ** (4 ** (2 / 2 * 2) / 2)
+print(a)
+a = -3 ** 2
+print(a)
+a = (-3) ** 2
+print(a)
+a = 3
+b = 4
+hipotenus = (a**2+ b**2)**0.5
+print (hipotenus)
+print(a, b, hipotenus)
+print(a, b, hipotenus, sep=' ----->')
+print(a)
+print(b)
+print (hipotenus)
+print (a, end ="bitiiiiiiiii" )
+print(b)
+print (hipotenus)
+bir = 'ilk satir'
+iki = 'ikinci satir'
+uc = 'ucuncu satir'
+print(bir,iki,uc)
+print(bir, iki, uc, sep='.')
+print(bir, iki, uc, sep='_____bosluk_____')
+print(bir)
+print(iki)
+print(uc)
+print(bir, end = "\n")
+print(iki, end = '\n')
+print(uc)
+print (bir, end = "\n**** ")
+print(iki, end = '\n')
+print(uc)
+print(bir,iki, uc, sep='* *', end = '&' )
+print (bir, iki, uc, sep=' ')
+print(bir, iki, uc, sep=' ')
+print(bir, iki, uc, sep='* - * ',end = '&\n')
+print(bir, iki,uc, sep=' ')
+print (bir, iki, uc, sep=' ')
+number = 2021
+text = "we have reached"
+print(text, number)
+text1 = "I bought"
+text2 = "kg. of apple this morning"
+amount = 6
+text3 = text1 + " " + str(amount) + " " + text2
+print(text1, amount, text2)
+print("I bought", 6, "kg. of apple this morning")
+print("I bought " + "6 " + "kg. of apple this morning")
+print(text3)
+print('abc', 555)
+text1 = "I bought"
+text3 = text1 + 'apple'
+print('C:\\north pole\noise_penguins.txt')
+print('----------')
+print('first', 'second', 'third', sep='\t')
+print('C:\\north pole\noise_penguins.txt')
+print('----------')
+print('first', 'second', 'third', sep='\t')
+text = 'Bu\gun\ben\neden\taksiye\bindim'
+print(text)
+print('Bu\gun\\ben\\neden\\taksiye\\þindim')
+print('we are', '\boosting', 'our', '\brotherhood')
+print('it\'s essential to learn Python\'s libraries in IT World')
+print(r' Bu\gun\ben\neden\taksiye\bindim')
+print('we are', '\boosting', 'our', '\brotherhood')
+print('it\'s essential to learn Python\'s libraries in IT World')
+bool_var = True and not True
+print(bool_var)
+print(2 and 3)
+print(1 and 0)
+print (2 and "hello world")
+print([] and "be happy!")
+print (None and ())
+print(2 or 3)
+print(None or 1)
+print(2 or "hello world")
+print([] or "be happy!")
+print (None or ())
+print({} or 0)
+print({0} or False)
+fruit = 'Orange'
+print('Word:', fruit)
+print('First letter:', fruit[0])
+print('Second letter:', fruit[1])
+print("3rd to 5th letters:", fruit[2:5])
+print("Letter all after 3rd:", fruit[2:])
